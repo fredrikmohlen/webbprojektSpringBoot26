30f116ff33fac2b54e198ab13e6344c77129da03
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-03-09 20:10:13 +0100

    creates BookService and begins to write metods in it. createBook and getBookById

---
+++ src/main/java/org/example/webbprojektspringboot26/books/BookMapper.java
@@ -0,0 +1,33 @@
+package org.example.webbprojektspringboot26.books;
+
+public class BookMapper {
+
+    private BookMapper() {}
+    
+    public static Book toEntity(CreateBookDTO dto) {
+        Book book = new Book();
+        book.setTitle(dto.getTitle());
+        book.setAuthor(dto.getAuthor());
+        book.setDescription(dto.getDescription());
+        book.setPublishDate(dto.getPublishDate());
+        book.setIsbn(dto.getIsbn());
+        return book;
+    }
+
+    public static BookViewDTO toViewDTO(Book book) {
+        BookViewDTO dto = new BookViewDTO();
+        dto.setId(book.getId());
+        dto.setTitle(book.getTitle());
+        dto.setAuthor(book.getAuthor());
+        dto.setDescription(book.getDescription());
+        dto.setPublishDate(book.getPublishDate());
+        return dto;
+    }
+
+    public static void updateEntity(Book book, UpdateBookDTO dto) {
+        book.setTitle(dto.getTitle());
+        book.setAuthor(dto.getAuthor());
+        book.setDescription(dto.getDescription());
+        book.setPublishDate(dto.getPublishDate());
+    }
+}
--- src/main/java/org/example/webbprojektspringboot26/books/BookService.java
+++ src/main/java/org/example/webbprojektspringboot26/books/BookService.java
@@ -1,4 +1,27 @@
 package org.example.webbprojektspringboot26.books;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public BookViewDTO createBook(CreateBookDTO createBookDTO) {
+        Book book = BookMapper.toEntity(createBookDTO);
+        Book savedBook = bookRepository.save(book);
+        return BookMapper.toViewDTO(savedBook);
+    }
+
+    public BookViewDTO getBookById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return BookMapper.toViewDTO(book);
+    }
 }
