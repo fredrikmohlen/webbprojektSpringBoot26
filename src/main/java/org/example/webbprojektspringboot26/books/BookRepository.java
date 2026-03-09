a4d4b74a19463f25b3df2e9e661d7c9fc17412d9
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-03-09 15:11:15 +0100

    creates BookRepository

---
+++ src/main/java/org/example/webbprojektspringboot26/books/BookRepository.java
@@ -0,0 +1,6 @@
+package org.example.webbprojektspringboot26.books;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface BookRepository extends ListCrudRepository<Book,Long> {
+}
