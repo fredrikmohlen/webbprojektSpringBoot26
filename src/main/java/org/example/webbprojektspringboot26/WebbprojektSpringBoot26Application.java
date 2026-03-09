87d70de47dda8451c184adb929081bd0ab338c07
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-03-09 12:00:22 +0100

    Initial commit

---
+++ src/main/java/org/example/webbprojektspringboot26/WebbprojektSpringBoot26Application.java
@@ -0,0 +1,13 @@
+package org.example.webbprojektspringboot26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbprojektSpringBoot26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbprojektSpringBoot26Application.class, args);
+	}
+
+}
---
+++ src/main/java/org/example/webbprojektspringboot26/books/BookService.java
@@ -0,0 +1,4 @@
+package org.example.webbprojektspringboot26.books;
+
+public class BookService {
+}
