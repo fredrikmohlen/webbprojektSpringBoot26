1ffd07558d7958ca9352070c7167452f259f4b30
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-03-18 17:00:17 +0100

    application.properties, change hibernate.ddl-auto=update. BookController, add injection of bookRepository
    Create jte-pages for create and list.
    
    add Flyway to pom.xml

--- src/main/java/org/example/webbprojektspringboot26/books/BookController.java
+++ src/main/java/org/example/webbprojektspringboot26/books/BookController.java
@@ -1,13 +1,21 @@
 package org.example.webbprojektspringboot26.books;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/books")
-    public String listBooks(){
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
         return "books/list";
     }
 
